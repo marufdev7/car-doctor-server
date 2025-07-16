e6187e8014dd6cfca6210d4dc3b21e78b426e62b
mrfhmd7 <mrf.hmd7@gmail.com>
2025-07-17 04:23:01 +0600

    post data into db in booking collection

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
@@ -40,12 +41,20 @@ async function run() {
             const query = { _id: new ObjectId(id) };
 
             const options = {
-                projection: {title: 1, service_id: 1, price:1, },
+                projection: { title: 1, img: 1, price: 1, },
             };
 
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
-        })
+        });
+
+        //bookings
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            // console.log(booking); 
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
