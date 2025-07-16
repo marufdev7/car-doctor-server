e28045f48e0f8c38e3c903329931f4dd6156dfcd
mrfhmd7 <mrf.hmd7@gmail.com>
2025-07-16 23:51:13 +0600

    get services details by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,7 +35,17 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
 
+            const options = {
+                projection: {title: 1, service_id: 1, price:1, },
+            };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
